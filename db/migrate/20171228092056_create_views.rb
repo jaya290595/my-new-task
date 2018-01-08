6d8f5313a713b0680013af3e369a5c85b719e23f
jaya290595 <jaya@maropost.com>
2018-01-08 12:54:40 +0530

    migration change

--- db/migrate/20171228092056_create_views.rb
+++ db/migrate/20171228092056_create_views.rb
@@ -1,7 +1,7 @@
 class CreateViews < ActiveRecord::Migration[5.1]
   def change
     create_table :views do |t|
-    	t.reference :movies
+    	t.references :movies
       t.timestamps
     end
   end
